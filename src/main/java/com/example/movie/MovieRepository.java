b2f279eee11ef387bc41662cb0b88c19f7c00a61
TechKumarNitish <rskban15a2015@gmail.com>
2024-03-09 11:33:43 +0000

    Initial Commit

---
+++ src/main/java/com/example/movie/MovieRepository.java
@@ -0,0 +1,21 @@
+// Write your code here
+
+package com.example.movie;
+
+import com.example.movie.Movie;
+import java.util.ArrayList;
+
+/**
+ * MovieRepository
+ */
+public interface MovieRepository {
+    ArrayList<Movie> getMovies();
+
+    Movie addMovie(Movie movie);
+
+    Movie getMovieById(int movieId);
+
+    Movie updateMovie(int movieId, Movie movie);
+
+    void deleteMovie(int movieId);
+}
